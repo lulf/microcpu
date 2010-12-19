3d04ae1d125079b61b5cf96b686ee396a4079081
Ulf Lilleengen <lulf@pvv.ntnu.no>
2010-12-19 15:18:45 +0100

    - Use input variable to make things cleaner. - Correct overflow check.

--- src/alu/alu_impl.vhdl
+++ src/alu/alu_impl.vhdl
@@ -21,28 +21,32 @@ begin  -- architecture rtl
   main: process (input) is
     variable status : std_logic_vector(STATUS_WIDTH - 1  downto 0);
     variable result : std_logic_vector(WIDTH - 1 downto 0);
+    variable input_a : std_logic_vector(WIDTH - 1 downto 0);
+    variable input_b : std_logic_vector(WIDTH - 1 downto 0);
   begin  -- process main
     -- Default values
-    status := X"0";
-    result := X"0";
+    status := (others => '0');
+    result := (others => '0');
+    input_a := input.in_a;
+    input_b := input.in_b;
     
     case input.in_op is
       when OP_AND =>
-        result := input.in_a and input.in_b;
+        result := input_a and input_b;
       when OP_OR =>
-        result := input.in_a or input.in_b;
+        result := input_a or input_b;
       when OP_ADD =>
-        result := std_logic_vector(signed(input.in_a) + signed(input.in_b));
-        if ((input.in_a'high = input.in_b'high) and (input.in_a'high /= result'high)) then
+        result := std_logic_vector(signed(input_a) + signed(input_b));
+        if input_a(input_a'high) = input_b(input_b'high) and input_a(input_a'high) /= result(result'high) then
           status := status or STATUS_OVERFLOW;
         end if;
       when OP_SUB =>
-        result := std_logic_vector(signed(input.in_a) - signed(input.in_b));
-        if ((input.in_a'high = input.in_b'high) and (input.in_a'high /= result'high)) then
+        result := std_logic_vector(signed(input_a) - signed(input_b));
+        if input_a(input_a'high) = input_b(input_b'high) and input_a(input_a'high) /= result(result'high) then
           status := status or STATUS_OVERFLOW;
         end if;
       when others =>
-        result := X"0";
+        result := (others => '0');
         status := status or STATUS_INVOP;
     end case;
     
